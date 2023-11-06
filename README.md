2c20333ca28b0db48b54b8145845d56bee2f94ef
Egor I <egor.ivanov@pm.me>
2023-11-06 18:22:52 +0300

    Iittial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # py-delete-testops-user-session
 delete Allure Testops user session data from redis database
+
+
+# Dependencies 
+`pip3 install redis PyJWT`
+
+# Env variable to be added
+
+`export REDIS_URL=redis://default:<redis_password>@<ip_or_fqdn>:6379"`
+
+
+# Usage
+```shell
+python3 main.py <username> [check|clean]
+```
---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import redis
+import jwt
+import time
+
+def scan_jwt(name, r, mode):
+    for key in r.scan_iter("allure:testops:jwt:*", 250):
+        token = r.get(key)
+        print(token)
+        try:
+            decoded = jwt.decode(token, options={"verify_signature": False})
+            user = decoded["sub"]
+            print("user:" + user)
+            exp = decoded["exp"]
+            print("expiration:" + exp)
+        except:
+            continue
+        if user == name:
+            ttl = r.ttl(key)
+            if ttl == -1 or int(time.time()) >= exp:
+                print(key, ttl, exp, )
+                if mode == "clean":
+                    r.delete(key)
+
+def scan(name, mode):
+    print(os.environ.get("REDIS_URL"))
+    r = redis.from_url(os.environ.get("REDIS_URL"))
+    scan_jwt(name, r, mode)
+
+if __name__ == '__main__':
+    scan(sys.argv[1], sys.argv[2] if sys.argv[2:] else "check")
